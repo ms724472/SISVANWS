cdc344e1f15f8811b4df989d6d8b933c28f6da7f
Edgar Tiburcio <edgar.tiburcio@oracle.com>
2021-02-15 18:03:37 -0600

    Agregando los cambios pendientes

--- src/java/iteso/sisvan/ws/SISVANWS.java
+++ src/java/iteso/sisvan/ws/SISVANWS.java
@@ -11,7 +11,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
